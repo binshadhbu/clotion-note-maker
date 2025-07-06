117a00852cc96500ca77491ed0c5dd7fa035a573
binshadhbu <binshadhubasheer@gmail.com>
2025-07-06 19:55:12 +0530

    added api to create document

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,18 +1,32 @@
 "use client";
 import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
 import { useUser } from '@clerk/nextjs';
+import { useMutation } from 'convex/react';
 import { PlusCircle } from 'lucide-react';
 import Image from 'next/image';
 import React from 'react'
+import { toast } from 'sonner';
 
 const Page = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({ title: "untitled" });
+    toast.promise(promise, {
+      loading: 'creating new note...',
+      success: 'note created successfully!',
+      error: 'failed to create note'
+    });
+  }
+
   return (
     <div className='h-full flex flex-col items-center justify-center space-y-4'>
       <Image src={'/empty.png'} alt='No documents' width={300} height={300} className='dark:hidden' />
       <Image src={'/empty-dark.png'} alt='No documents' width={300} height={300} className='dark:block hidden' />
       <h2 className='text-lg font-medium'>Welcome to {user?.firstName}&apos;s workspace</h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className='h-4 w-4 mr-2' />
         New Document
       </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
       >
         <ConvexClientProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange storageKey="motion-theme">
+            <Toaster position="bottom-center"/>
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
